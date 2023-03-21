30e1881754cca4b7f5082d93b36eab3790c5f744
hilal.coskun <hilal.coskun20@gmail.com>
2023-03-21 13:11:32 +0300

    added admin userList page action method

---
+++ IdentityApp.Web/IdentityApp.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using IdentityApp.Web.Areas.Admin.Models;
+using IdentityApp.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApp.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+
+        private readonly UserManager<AppUser> _userManager;
+
+        public HomeController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> UserList()
+        {
+            var users = await _userManager.Users
+                .ToListAsync();
+
+            var userViewList = users
+                .Select(x => new UserViewModel()
+                {
+                    Name = x.UserName,
+                    Id = x.Id,
+                    Email = x.Email
+                }).ToList();
+
+            return View(userViewList);
+        }
+    }
+}
---
+++ IdentityApp.Web/IdentityApp.Web/Areas/Admin/Models/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace IdentityApp.Web.Areas.Admin.Models
+{
+    public class UserViewModel
+    {
+        public string? Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+
+    }
+}
