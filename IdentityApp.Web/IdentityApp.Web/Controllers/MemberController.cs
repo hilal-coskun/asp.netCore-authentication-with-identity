c423bc944a5c4b83ba24e2d7cf3959eb4ce831f2
hilal.coskun <hilal.coskun20@gmail.com>
2023-03-31 14:39:06 +0300

    edit user action

--- IdentityApp.Web/IdentityApp.Web/Controllers/MemberController.cs
+++ IdentityApp.Web/IdentityApp.Web/Controllers/MemberController.cs
@@ -4,6 +4,8 @@ using IdentityApp.Web.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.FileProviders;
 
 namespace IdentityApp.Web.Controllers
 {
@@ -13,11 +15,13 @@ namespace IdentityApp.Web.Controllers
 
         private readonly SignInManager<AppUser> _signInManager;
         private readonly UserManager<AppUser> _userManager;
+        private readonly IFileProvider _fileProvider;
 
-        public MemberController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
+        public MemberController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager, IFileProvider fileProvider)
         {
             _signInManager = signInManager;
             _userManager = userManager;
+            _fileProvider = fileProvider;
         }
 
         public async Task<IActionResult> Index()
@@ -28,7 +32,8 @@ namespace IdentityApp.Web.Controllers
             {
                 Email = user.Email == null ? " " : user.Email,
                 PhoneNumber = user.PhoneNumber == null ? " " : user.PhoneNumber,
-                UserName = user.UserName == null ? " " : user.UserName
+                UserName = user.UserName == null ? " " : user.UserName,
+                PictureUrl = user.Picture
             };
 
             return View(userViewModel);
@@ -78,6 +83,80 @@ namespace IdentityApp.Web.Controllers
             TempData["SuccessMessage"] = "Şifreniz başarıyla değiştirilmiştir!";
 
             return View();
+        }
+
+        public async Task<IActionResult> UserEdit()
+        {
+            ViewBag.genderList = new SelectList(Enum.GetNames(typeof(Gender)));
+
+            var currentUser = (await _userManager.FindByNameAsync(User.Identity!.Name))!;
+
+            var userEditViewModel = new UserEditViewModel()
+            {
+                UserName = currentUser.UserName,
+                Email = currentUser.Email,
+                Phone = currentUser.PhoneNumber,
+                City = currentUser.City,
+                BirthDate = currentUser.BirthDate,
+                Gender = currentUser.Gender
+            };
+             
+            return View(userEditViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserEdit(UserEditViewModel request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var currentUser = await _userManager.FindByNameAsync(User.Identity!.Name);
+            currentUser.UserName = request.UserName;
+            currentUser.Email = request.Email;
+            currentUser.BirthDate = request.BirthDate;
+            currentUser.City = request.City;
+            currentUser.Gender = request.Gender;
+            currentUser.PhoneNumber = request.Phone;
+
+            if (request.Picture != null && request.Picture.Length > 0)
+            {
+                var wwwrootFolder = _fileProvider.GetDirectoryContents("wwwroot");
+                string randomFileName = $"{Guid.NewGuid().ToString()}{Path.GetExtension(request.Picture.FileName)}";
+
+                var newPicturePath = Path.Combine(wwwrootFolder.First(x => x.Name == "userpictures").PhysicalPath, randomFileName);
+                using var stream = new FileStream(newPicturePath, FileMode.Create);
+                await request.Picture.CopyToAsync(stream);
+
+                currentUser.Picture = randomFileName;
+            }
+
+            var updateToUserResult = await _userManager.UpdateAsync(currentUser);
+
+            if (!updateToUserResult.Succeeded)
+            {
+                ModelState.AddModelErrorList(updateToUserResult.Errors.Select(x => x.Description).ToList());
+                return View();
+            }
+
+            await _userManager.UpdateSecurityStampAsync(currentUser);
+            await _signInManager.SignOutAsync();
+            await _signInManager.SignInAsync(currentUser,true);
+
+            TempData["SuccessMessage"] = "Kullanıcı  bilgileri başarıyla değiştirilmiştir!";
+
+            var userEditViewModel = new UserEditViewModel()
+            {
+                UserName = currentUser.UserName,
+                Email = currentUser.Email,
+                BirthDate = currentUser.BirthDate,
+                City = currentUser.City,
+                Gender = currentUser.Gender,
+                Phone = currentUser.PhoneNumber
+            };
+
+            return View(userEditViewModel);
         }
     }
 }
