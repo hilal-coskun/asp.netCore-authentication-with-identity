d3e83ac593d1a3223256fd0dd20b0af693f6bc90
hilal.coskun <hilal.coskun20@gmail.com>
2023-03-31 14:39:23 +0300

    added tag-helper

---
+++ IdentityApp.Web/IdentityApp.Web/TagHelpers/UserPictureThumbnailTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace IdentityApp.Web.TagHelpers
+{
+    public class UserPictureThumbnailTagHelper : TagHelper
+    {
+        public string? PictureUrl { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "img";
+
+            if (string.IsNullOrEmpty(PictureUrl))
+            {
+                output.Attributes.SetAttribute("src", "/userpictures/default_user_picture.jpeg");
+            }
+            else
+            {
+                output.Attributes.SetAttribute("src", $"/userpictures/{PictureUrl}");
+            }
+        }
+    }
+}
