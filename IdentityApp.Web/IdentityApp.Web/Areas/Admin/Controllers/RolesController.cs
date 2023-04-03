8ef5e0bf50d1f5d444290ed004c93095306eb98e
hilal.coskun <hilal.coskun20@gmail.com>
2023-04-03 11:11:47 +0300

    added role create feature

---
+++ IdentityApp.Web/IdentityApp.Web/Areas/Admin/Controllers/RolesController.cs
@@ -0,0 +1,55 @@
+﻿using IdentityApp.Web.Areas.Admin.Models;
+using IdentityApp.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using IdentityApp.Web.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApp.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RolesController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RolesController(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var roles = await _roleManager.Roles.Select(x => new RolViewModel()
+            {
+                Name = x.Name,
+                Id = x.Id
+            }).ToListAsync();
+
+            return View(roles);
+        }
+
+        public IActionResult RoleCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RoleCreate(RoleCreateViewModel request)
+        {
+            var result = await _roleManager.CreateAsync(new AppRole()
+            {
+                Name = request.Name,
+            });
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelErrorList(result.Errors.Select(x => x.Description).ToList());
+            }
+
+            return RedirectToAction(nameof(RolesController.Index));
+        }
+
+    }
+}
---
+++ IdentityApp.Web/IdentityApp.Web/Areas/Admin/Models/RoleCreateViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityApp.Web.Areas.Admin.Models
+{
+    public class RoleCreateViewModel
+    {
+        [Required(ErrorMessage = "Rol isim alanı boş bırakılamaz!")]
+        [Display(Name = "Rol isim alanı: ")]
+        public string Name { get; set; }
+    }
+}
