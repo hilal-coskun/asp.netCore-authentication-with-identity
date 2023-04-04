f6afecd39dca0f33f56f497e151b082faeb55e7e
hilal.coskun <hilal.coskun20@gmail.com>
2023-04-04 23:26:06 +0300

    added role created and updated action

--- IdentityApp.Web/IdentityApp.Web/Areas/Admin/Controllers/RolesController.cs
+++ IdentityApp.Web/IdentityApp.Web/Areas/Admin/Controllers/RolesController.cs
@@ -48,7 +48,93 @@ namespace IdentityApp.Web.Areas.Admin.Controllers
                 ModelState.AddModelErrorList(result.Errors.Select(x => x.Description).ToList());
             }
 
+            TempData["SuccessMessage"] = "Rol oluşturulmuştur!";
             return RedirectToAction(nameof(RolesController.Index));
+        }
+
+        public async Task<IActionResult> RoleUpdate(string id)
+        {
+            var roleToUpdate = await _roleManager.FindByIdAsync(id);
+
+            if (roleToUpdate == null)
+            {
+                throw new Exception("Güncellenecek rol bulunamamıştır!");
+            }
+
+            var newRole = new RoleUpdateViewModel()
+            {
+                Id = roleToUpdate.Id,
+                Name = roleToUpdate.Name
+            };
+            
+            return View(newRole);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RoleUpdate(RoleUpdateViewModel request)
+        {
+            var roleToUpdate = await _roleManager.FindByIdAsync(request.Id);
+
+            if (roleToUpdate == null)
+            {
+                throw new Exception("Güncellenecek rol bulunamamıştır!");
+            }
+
+            roleToUpdate.Name = request.Name;
+
+            await _roleManager.UpdateAsync(roleToUpdate);
+
+            ViewData["SuccessMessage"] = "Rol bilgisi güncellenmiştir";
+
+            return View();
+        }
+
+        public async Task<IActionResult> RoleDelete(string Id)
+        {
+            var role = await _roleManager.FindByIdAsync(Id);
+
+            if (role == null)
+            {
+                throw new Exception("Silinecek rol bulunamamıştır");
+            }
+
+            var result = await _roleManager.DeleteAsync(role);
+
+            if (!result.Succeeded)
+            {
+                throw new Exception(result.Errors.Select(x => x.Description).First());
+            }
+
+            TempData["SuccessMessage"] = "Rol silinmiştir!";
+
+            return RedirectToAction(nameof(RolesController.Index));
+        }
+
+        public async Task<IActionResult> AssignRoleToUser(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            var roles = await _roleManager.Roles.ToListAsync();
+            var roleViewModel = new List<AssignRoleToUserViewModel>();
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            foreach (var role in roles)
+            {
+                var assignRoleToUserViewModel = new AssignRoleToUserViewModel()
+                {
+                    Id = role.Id,
+                    Name = role.Name
+                };
+
+                if (userRoles.Contains(role.Name))
+                {
+                    assignRoleToUserViewModel.Exist = true;
+                }
+
+                roleViewModel.Add(assignRoleToUserViewModel);
+
+            }
+
+            return View(roleViewModel);
         }
 
     }
--- IdentityApp.Web/IdentityApp.Web/Areas/Admin/Models/RoleUpdateViewModel.cs
+++ IdentityApp.Web/IdentityApp.Web/Areas/Admin/Models/RoleUpdateViewModel.cs
@@ -2,10 +2,13 @@
 
 namespace IdentityApp.Web.Areas.Admin.Models
 {
-    public class RoleCreateViewModel
+    public class RoleUpdateViewModel
     {
+        public string Id { get; set; } = null!;
+
         [Required(ErrorMessage = "Rol isim alanı boş bırakılamaz!")]
         [Display(Name = "Rol ismi: ")]
         public string Name { get; set; }
+
     }
 }
